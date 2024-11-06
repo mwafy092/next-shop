3dc9f201ad9daeddc7e4b3b2f990dca394c4394f
Mustafa Mwafy <mwafy@Mustafas-MacBook-Air.local>
2024-11-06 14:25:52 +0200

    add navbar in shared components folder

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-    return <div>Welcome to the next store</div>;
+    return <></>;
 };
 
 export default Home;
---
+++ src/app/(shared-components)/nav.tsx
@@ -0,0 +1,37 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+const Nav = () => {
+    const [cart, setCart] = useState([]);
+    return (
+        <nav className='bg-green-800 text-white h-16 p-4 flex justify-between items-center px-20'>
+            <div className='flex items-center gap-20'>
+                <Link
+                    href='/'
+                    className='flex items-center gap-2'>
+                    <Image
+                        src='/logo.png'
+                        width={50}
+                        height={50}
+                        alt='next cart logo'
+                    />
+                    <span className='font-bold'>Next Shop</span>
+                </Link>
+                <button className='font-bold'>Home</button>
+            </div>
+            <button className='flex gap-2 items-center'>
+                <Image
+                    src='/icons/cart.png'
+                    width={32}
+                    height={32}
+                    alt='cart icon'
+                />
+                <span>{cart.length}</span>
+            </button>
+        </nav>
+    );
+};
+
+export default Nav;
